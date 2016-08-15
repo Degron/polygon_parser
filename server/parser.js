672b9240f1163c7192f6c4bd722fd385bdd5bf99
Evgenii Bezpalko <ebezpalk@cablevision.com>
2016-08-15 18:30:03 -0400

    lived only Polygon

--- server/parser.js
+++ server/parser.js
@@ -15,10 +15,10 @@ r.connect(config.rethinkdb, function (err, conn) {
 })
 
 var getStream = function () {
-    var jsonData = '../../zcta5.json',
-    // var jsonData = __dirname +'/test.json',
+    // var jsonData = '../../zcta5.json',
+        var jsonData = __dirname +'/test.json',
         stream = fs.createReadStream(jsonData, { encoding: 'utf8' }),
-        parser = JSONStream.parse([true, {emitKey: true}]);
+        parser = JSONStream.parse([true, { emitKey: true }]);
     return stream.pipe(parser);
 }
 
@@ -29,30 +29,31 @@ function startParsing(_rdbConn) {
         var longitude = []
         console.log(data.value.properties.ZCTA5CE10)
         console.log(data.value.geometry.coordinates)
+        if (data.value.geometry.type === 'Polygon') {
+            data.value.geometry.coordinates.forEach(function (item) {
+                latitude.push(item[0])
+                longitude.push(item[1])
+            })
 
-        data.value.geometry.coordinates.forEach(function (item) {
-            latitude.push(item[0])
-            longitude.push(item[1])
-        })
+            meow = {}
+            var zip_code = data.value.properties.ZCTA5CE10
+            var polygon = [[latitude.max(), longitude.min()], [latitude.max(), longitude.max()], [latitude.min(), longitude.max()], [latitude.min(), longitude.min()], [latitude.max(), longitude.min()]]
+            meow[zip_code] = polygon
 
-        meow = {}
-        var zip_code = data.value.properties.ZCTA5CE10
-        var polygon = [[latitude.max(), longitude.min()], [latitude.max(), longitude.max()], [latitude.min(), longitude.max()], [latitude.min(), longitude.min()], [latitude.max(), longitude.min()]]
-        meow[zip_code] = polygon
-        // console.log(meow)
-        try {
-            // var todo = yield parse(this);
-            // todo.createdAt = r.now(); // Set the field `createdAt` to the current time
-            var result = r.table('zip_code_polygon').insert({'zip_code': zip_code, 'polygon': polygon} , { returnChanges: true }).run(_rdbConn);
+            try {
+                // var todo = yield parse(this);
+                // todo.createdAt = r.now(); // Set the field `createdAt` to the current time
+                var result = r.table('zip_code_polygon').insert({ 'zip_code': zip_code, 'polygon': polygon }, { returnChanges: true }).run(_rdbConn);
 
-            // todo = result.changes[0].new_val; // todo now contains the previous todo + a field `id` and `createdAt`
-            // this.body = JSON.stringify(todo);
+                // todo = result.changes[0].new_val; // todo now contains the previous todo + a field `id` and `createdAt`
+                // this.body = JSON.stringify(todo);
+            }
+            catch (e) {
+                // this.status = 500;
+                // this.body = e.message || http.STATUS_CODES[this.status];
+            }
         }
-        catch (e) {
-            this.status = 500;
-            this.body = e.message || http.STATUS_CODES[this.status];
-        }
-        
+
     })
 
 }
